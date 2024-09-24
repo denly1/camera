ba89c8b7e3b1fc484eddaf3c9e6fae668cabfb27
Sasha <99606517+denly1@users.noreply.github.com>
2024-09-24 20:32:15 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.camera;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageView profileIv;
+    Button cameraBtn, galleryBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        profileIv = findViewById(R.id.profile_iv);
+        cameraBtn = findViewById(R.id.camera_btn);
+        galleryBtn = findViewById(R.id.gallery_btn);
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraResultLauncher.launch(cameraIntent);
+            }
+        });
+
+    }
+
+    ActivityResultLauncher<Intent> cameraResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Bitmap avatar = (Bitmap) result.getData().getExtras().get("data");
+                        profileIv.setImageBitmap(avatar);
+                    }
+                }
+            }
+    );
+}
